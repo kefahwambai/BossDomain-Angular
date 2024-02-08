38f3325c691ad56675e0e08dca78c32f1a865e5c
KefahWambai <kefah.ngwei@student.moringaschool.com>
2024-02-08 23:36:05 +0300

    added cart icon update

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,5 +1,6 @@
 import { Component, HostListener } from '@angular/core';
 import { Router } from '@angular/router';
+import { CartService } from '../../service/cart.service'; 
 
 @Component({
   selector: 'app-navbar',
@@ -7,20 +8,23 @@ import { Router } from '@angular/router';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent {
-
-
-  constructor(private router: Router) { }
-
   isScrolled = false;
+  cartItemCount = 0;  
+
+  constructor(private router: Router, public cartService: CartService) {}
 
   @HostListener('window:scroll', [])
   onWindowScroll() {
     this.isScrolled = window.scrollY > 0;
   }
 
+  ngOnInit() {
+    this.cartService.cartItems$.subscribe(items => {
+      this.cartItemCount = items.length;
+    });
+  }
 
   navigateTo(route: string): void {
     this.router.navigate([route]);
-   
   }
 }
